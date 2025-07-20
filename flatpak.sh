c59d23b6afd17e97e65632c39e8baf27ce9845ba
Vadim Totok <112573798+VadimTotok@users.noreply.github.com>
2025-07-20 14:49:31 +0300

    Create flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,13 @@
+echo "Install flatpak apps..."
+
+flatpak install -y \
+	com.brave.Browser \
+	org.telegram.desktop \
+	com.mattjakeman.ExtensionManager \
+	org.onlyoffice.desktopeditors \
+	org.qbittorrent.qBittorrent \
+	md.obsidian.Obsidian \
+	com.bitwarden.desktop \
+	org.kde.krita \
+	org.kde.kdenlive \
+	org.gnome.Extensions \
