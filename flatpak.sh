c751e5f6d39983df19fb51639e937847a123b1b1
Vadim Totok <112573798+VadimTotok@users.noreply.github.com>
2026-02-02 09:34:14 +0300

    Refactor: optimize flatpak installation script

--- flatpak.sh
+++ flatpak.sh
@@ -1,24 +1,27 @@
+set -e
+
 echo "Install flatpak..."
 
-sudo apt install flatpak
-sudo apt install gnome-software-plugin-flatpak
+echo "Updating system and installing Flatpak..."
+sudo apt update
+sudo apt install flatpak -y
+
+echo "Adding Flathub repository..."
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-echo "Install flatpak apps..."
+APPS=(
+  com.brave.Browser
+  org.telegram.desktop
+  org.onlyoffice.desktopeditors
+  org.qbittorrent.qBittorrent
+  md.obsidian.Obsidian
+  com.bitwarden.desktop
+  org.kde.krita
+  com.teamspeak.TeamSpeak
+  com.discordapp.Discord
+  com.obsproject.Studio
+)
 
-flatpak install -y \
-	com.brave.Browser \
-	org.telegram.desktop \
-	com.mattjakeman.ExtensionManager \
-	org.onlyoffice.desktopeditors \
-	org.qbittorrent.qBittorrent \
-	md.obsidian.Obsidian \
-	com.bitwarden.desktop \
-	org.kde.krita \
-	org.kde.kdenlive \
-	org.gnome.Extensions \
-	com.teamspeak.TeamSpeak \
-	org.gnome.Totem \
-	com.discordapp.Discord \
-	com.obsproject.Studio \
-	
+echo "Install flatpak apps..."
+flatpak install -y flathub "${APPS[@]}"
+echo "Done! Restart your system if the app icons dont appear."
